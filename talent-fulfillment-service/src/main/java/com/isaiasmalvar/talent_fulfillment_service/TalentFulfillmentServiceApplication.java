89de4adaf42cfbca9e5dce1c1bc9d90cd5b7b882
Isaias Malvar <isaiasmg@93hotmail.com>
2024-12-11 13:48:24 +0000

    Set up configuration for all microservices

--- talent-fulfillment-service/src/main/java/com/isaiasmalvar/talent_fulfillment_service/TalentFulfillmentServiceApplication.java
+++ talent-fulfillment-service/src/main/java/com/isaiasmalvar/talent_fulfillment_service/TalentFulfillmentServiceApplication.java
@@ -1,10 +1,29 @@
 package com.isaiasmalvar.talent_fulfillment_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class TalentFulfillmentServiceApplication {
+
+	@Value("${spring.application.name}")
+	private String serviceName;
+
+	@Autowired
+	private Environment environment;
+
+	@GetMapping("/test")
+	public String getTest() {
+		return "testing " + serviceName + " at port: " + environment.getProperty("local.server.port");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TalentFulfillmentServiceApplication.class, args);
